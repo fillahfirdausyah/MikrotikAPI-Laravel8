7432c166c727dd4da144bcb9c4818077fd115f4b
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2020-11-20 20:47:39 +0700

    add logo

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -61,7 +61,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{ '/home' }}" class="brand-link text-sm">
-      <img src="{{ asset('dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+      <img src="{{ asset('logo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
       <span class="brand-text font-weight-light">Lugaru</span>
     </a>
@@ -71,7 +71,7 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="{{ asset('dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
+          <img src="{{ asset('logo.png') }}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block">Lugaru</a>
